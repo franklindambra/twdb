95c57efc15561e9dca8c354b6e11d66630970b16
Franklin Dambra <franklindambra@gmail.com>
2025-03-13 10:56:06 -0500

    g

---
+++ components/Tag.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Tag() {
+  return (
+    <div className="py-1 px-4 rounded-full shadow-lg inline-block border-2 min-w-32 text-center font-mono font-bold">
+      Tag
+    </div>
+  );
+}
