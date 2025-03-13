8a6aa0b797d9093e069d157cb824ed3943bd58d2
Franklin Dambra <franklindambra@gmail.com>
2025-03-13 11:02:55 -0500

    win

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="py-20 md:py-30 bg-gradient-to-b from-neutral-950 to-neutral-800 text-white text-center">
+    <div className="py-20 md:py-30 bg-linear-to-b from-neutral-950 to-neutral-800 text-white text-center">
       <p className="font-bold text-4xl mb-5">Test paragraph.</p>
       <p className="font-mono mb-5">roboto mono</p>
       <Tag />
